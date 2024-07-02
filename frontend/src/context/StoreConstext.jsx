960600337f567749c37581994e90ddfda48fdd64
Aman Kumar Singh <aman20012004@gmail.com>
2024-07-03 00:10:51 +0530

    updated

--- frontend/src/context/StoreConstext.jsx
+++ frontend/src/context/StoreConstext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://zoopo-server.onrender.com";
+    const url = "http://localhost:4000";
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([])
 
