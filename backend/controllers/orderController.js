8a2ea46ce9d189c6f314b77948cc191f02e0ff49
Aman Kumar Singh <aman20012004@gmail.com>
2024-07-02 22:37:27 +0530

    updated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order from frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "https://zoopo.vercel.app/";
+    const frontend_url = "https://zoopo.vercel.app";
 
     try {
         const newOrder = new orderModel({
